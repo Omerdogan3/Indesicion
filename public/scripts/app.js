8260a8a83e5c2af7dde308b2b56fa7ba602f1e9c
Omer Dogan <omerdogan3@gmail.com>
2017-10-15 17:22:59 +0300

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,72 +1,83 @@
-"use strict";
+'use strict';
 
+/*commands:
+babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+live-server public
+*/
 console.log('App.js is running');
+
+var app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: ['One', 'Two']
+};
 
 // JSX - Javascript XML
 var template = React.createElement(
-	"div",
+	'div',
 	null,
 	React.createElement(
-		"h1",
+		'h1',
 		null,
-		"This is JSX!"
+		app.title
+	),
+	app.subtitle && React.createElement(
+		'p',
+		null,
+		app.subtitle
 	),
 	React.createElement(
-		"p",
+		'p',
 		null,
-		"Hello"
+		app.options.length >= 2 ? 'Here are your options' : 'No Options'
 	),
 	React.createElement(
-		"ol",
+		'ol',
 		null,
 		React.createElement(
-			"li",
+			'li',
 			null,
-			"Item One"
+			app.options[0]
 		),
 		React.createElement(
-			"li",
+			'li',
 			null,
-			"Item Two"
+			app.options[1]
 		)
 	)
 );
-/*
-var template = React.createElement(
-    "h1",
-    { id: "app" },
-    "Hello React"
-);*/
-
 var user = {
 	name: "Omer",
 	age: 22,
-	loc: "istanbul"
+	loc: "Istanbul"
 };
 
-var userName = 'Omer';
-var userAge = 22;
-var userLocation = 'Istanbul';
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			'p',
+			null,
+			'Location: ',
+			location
+		);
+	}
+}
+
 var template2 = React.createElement(
-	"div",
+	'div',
 	null,
 	React.createElement(
-		"h1",
+		'h1',
 		null,
-		user.name
+		user.name ? user.name : 'Anonymous'
 	),
 	React.createElement(
-		"p",
+		'p',
 		null,
-		"Age:",
+		'Age:',
 		user.age
 	),
-	React.createElement(
-		"p",
-		null,
-		"Location: ",
-		user.loc
-	)
+	getLocation(user.loc)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -2,43 +2,48 @@
 babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 live-server public
 */
-
 console.log('App.js is running');
+
+var app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: ['One','Two']
+};
 
 // JSX - Javascript XML
 var template = (
 	<div>
-		<h1>This is JSX!</h1>
-		<p>Hello</p>
+		<h1>{app.title}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.options.length >= 2 ? 'Here are your options' : 'No Options'}</p>
 		<ol>
-			<li>Item One</li>
-			<li>Item Two</li>
+			<li>{app.options[0]}</li>
+			<li>{app.options[1]}</li>
 		</ol>
 	</div>
 );  
-/*
-var template = React.createElement(
-    "h1",
-    { id: "app" },
-    "Hello React"
-);*/
-
 var user = {
 	name : "Omer",
 	age : 22,
-	loc : "istanbul"
+	loc : "Istanbul"
 };
 
-var userName = 'Omer';
-var userAge = 22;
-var userLocation = 'Istanbul';
+function getLocation(location){
+	if(location){
+		return <p>Location: {location}</p>;
+	}	
+}
+
 var template2 =(
 	<div>
-		<h1>{user.name}</h1>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
 		<p>Age:{user.age}</p>
-		<p>Location: {user.loc}</p>
+		{getLocation(user.loc)}
 	</div>
 );
+
+
+
 
 var appRoot = document.getElementById('app');
 var nameRoot = document.getElementById('name');
