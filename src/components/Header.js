b7b156d2ece7eca341ebf27788dd4c04ea0c84ff
Omer Dogan <omerdogan3@gmail.com>
2017-10-31 15:49:20 +0300

    style body

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Header = (props) =>{
 	return (
 		<div className="header">
-			<h1 className="header__title">{props.title}</h1>
-			{props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+			<div className="container">
+				<h1 className="header__title">{props.title}</h1>
+				{props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+			</div>
 		</div>
 	);
 }
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -95,24 +95,26 @@ export default class IndecisionApp extends React.Component{
 		return(
 			<div>
 				<Header subtitle = {subtitle}/>
-				<Action
-					hasOptions={this.state.options.length > 0}
-					handlePick={this.handlePick}				
-				/>
-				<Options 
-					options={this.state.options}
-					handleDeleteOptions = {this.handleDeleteOptions}
-					handleDeleteOption = {this.handleDeleteOption}
-				/>
-				<AddOption
-					handleAddOption = {this.handleAddOption}
-				/>
-
+				<div className="container">
+					<Action
+						hasOptions={this.state.options.length > 0}
+						handlePick={this.handlePick}				
+					/>
+					<Options 
+						options={this.state.options}
+						handleDeleteOptions = {this.handleDeleteOptions}
+						handleDeleteOption = {this.handleDeleteOption}
+					/>
+					<AddOption
+						handleAddOption = {this.handleAddOption}
+					/>
+				</div>
+					
 				<OptionModal
 					selectedOption={this.state.selectedOption}
 					closeModal={this.handleCloseModal}
 				/>
-
+				
 			</div>
 		);
 	}
